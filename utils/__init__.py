cff6e7431deb4679fff18d6cb0924c3c5dbedc22
MelOzyurt <mozyurt2000@gmail.com>
2025-07-31 23:12:08 +0100

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+# utils/__init__.py
+
+# This file makes the 'utils' folder a Python package.
+# You can optionally import modules here for easier access.
+
+from .doc_parser import parse_document
+from .summarizer import generate_summary
